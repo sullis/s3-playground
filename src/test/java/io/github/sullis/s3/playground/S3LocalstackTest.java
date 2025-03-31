7035c62eccdb05bc4a09184ea9e4a4ed59cabf0d
sullis <seans@grubhub.com>
2025-03-30 23:21:11 -0700

    restore S3LocalstackTest

--- src/test/java/io/github/sullis/s3/playground/S3LocalstackTest.java
+++ src/test/java/io/github/sullis/s3/playground/S3LocalstackTest.java
@@ -1,6 +1,5 @@
 package io.github.sullis.s3.playground;
 
-import org.junit.jupiter.api.Disabled;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -8,7 +7,6 @@ import org.testcontainers.utility.DockerImageName;
 
 
 @Testcontainers
-@Disabled
 public class S3LocalstackTest extends AbstractS3Test {
   @Container
   private static final LocalStackContainer LOCALSTACK = new LocalStackContainer(DockerImageName.parse("localstack/localstack:s3-latest"))
