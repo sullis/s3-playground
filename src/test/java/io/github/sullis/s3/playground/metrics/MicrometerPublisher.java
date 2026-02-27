36c344f9b04bda124412aa8e9c53c3d29f39d09b
sullis <seans@grubhub.com>
2026-02-27 15:51:34 -0800

    Checkpoint from Copilot CLI for coding agent session

--- src/test/java/io/github/sullis/s3/playground/metrics/MicrometerPublisher.java
+++ src/test/java/io/github/sullis/s3/playground/metrics/MicrometerPublisher.java
@@ -1,12 +1,16 @@
 package io.github.sullis.s3.playground.metrics;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.metrics.MetricCollection;
 import software.amazon.awssdk.metrics.MetricPublisher;
 import software.amazon.awssdk.metrics.SdkMetric;
 
 public class MicrometerPublisher
     implements MetricPublisher {
+  private static final Logger logger = LoggerFactory.getLogger(MicrometerPublisher.class);
+
   // visible for testing
   public final MeterRegistry registry;
 
@@ -18,15 +22,11 @@ public class MicrometerPublisher
   public void publish(MetricCollection metricCollection) {
     // TODO : WIP
     String name = metricCollection.name();
-    System.out.println("publish called. name=" + name);
+    logger.info("publish called. name={}", name);
     metricCollection.stream().map(metricRecord -> {
-      System.out.println("metricRecord: " + metricRecord);
+      logger.info("metricRecord: {}", metricRecord);
       SdkMetric sdkMetric = metricRecord.metric();
-      System.out.println("name=" + name
-          + " sdkMetric="
-          + sdkMetric
-          + " level="
-          + sdkMetric.level());
+      logger.info("name={} sdkMetric={} level={}", name, sdkMetric, sdkMetric.level());
       return metricRecord;
     }).toList();
   }
